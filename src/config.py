9510fe604edddccfc519b519f46c1d2a8b1faa71
kriskim8937 <kriskim8937@gmail.com>
2024-06-05 18:00:38 +0200

    Refactor main.py
    Refactor main.py

--- src/config.py
+++ src/config.py
@@ -2,3 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE_PATH = os.path.join(BASE_DIR, "..", "data", "news.db")
+os.environ["IMAGEMAGICK_BINARY"] = (
+    "C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"  # Update the path accordingly
+)  
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 from gpt_4 import get_gpt4_response
 from dall_e_3 import DallE3, save_image
 from tts_1 import generate_audio
@@ -15,109 +16,152 @@ from models import (
     process_news,
     execute_query,
 )
-import os
-
-os.environ["IMAGEMAGICK_BINARY"] = (
-    "C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"  # Update the path accordingly
-)  
 
 
 def translate_and_summarize(news):
-    prompt = f"The article titled {news.title} is provided below. translate into Korean with honorifics and summarized in 5 sentences, eliminate any irrelevant details. Don't use dash '-'. Should be less than 290 characters:\n\n{news.content}"
+    """
+    Translates and summarizes the news content into Korean with honorifics in less than 290 characters. 
+
+    Args:
+    news (News): News object containing the title and content.
+
+    Returns:
+    str: Translated and summarized content.
+    """
+    prompt = f"The article titled {news.title} is provided below. Translate into Korean with honorifics and summarize in 5 sentences, eliminating any irrelevant details. Don't use dash '-'. Should be less than 290 characters in total:\n\n{news.content}"
     return get_gpt4_response(prompt)
 
 
 def get_new_title(title, updated_news):
-    select_query = """
-    SELECT new_title
-    FROM svt_news
-    WHERE title = ?;
     """
+    Retrieves or generates a new title for the news in Korean.
+
+    Args:
+    title (str): Original title of the news.
+    updated_news (str): Updated news content.
+
+    Returns:
+    str: New title in Korean.
+    """
+    select_query = "SELECT new_title FROM svt_news WHERE title = ?;"
     new_title = execute_query(select_query, (title,))
+
     if new_title:
         new_title = new_title[0][0]
-    print(new_title)
+
     if not new_title:
-        prompt = f"Generate a title of the below news in Korean in one sentence. Should be less then 18 characters:\n\n{updated_news}"
+        prompt = f"Generate a title of the below news in Korean in one sentence. Should be less than 18 characters. Don't use special characters that are not allowed in file name:\n\n{updated_news}"
         new_title = get_gpt4_response(prompt)
-        execute_query("""
-            UPDATE svt_news
-            SET new_title = ?
-            WHERE title = ?
-        """, (new_title, title))
+        execute_query("UPDATE svt_news SET new_title = ? WHERE title = ?", (new_title, title))
+
     return new_title
 
 
-def get_revised_prompt(prompt):
-    prompt = "Remove sensitive words from below sentence:\n\n" + prompt
-    return get_gpt4_response(prompt)
+def split_text(text):
+    """
+    Splits the text into two parts roughly equal in length.
+
+    Args:
+    text (str): Text to be split.
+
+    Returns:
+    tuple: Two parts of the text.
+    """
+    middle_index = len(text) // 2
+
+    before = text.rfind(' ', 0, middle_index)
+    after = text.find(' ', middle_index)
+
+    if before == -1:
+        split_index = after
+    elif after == -1:
+        split_index = before
+    else:
+        split_index = before if middle_index - before <= after - middle_index else after
+
+    part1 = text[:split_index]
+    part2 = text[split_index+1:]
+
+    return part1, part2
+
+
+def read_unprocessed_news():
+    """
+    Reads unprocessed news from the database.
+
+    Returns:
+    list: List of unprocessed news objects.
+    """
+    query = "SELECT title, url FROM svt_news WHERE processed = 0"
+    return [News(title, "", url) for title, url in execute_query(query)]
 
 
 def main():
+    """
+    Main function to orchestrate the news processing.
+    """
     news_list = get_news_titles_and_urls()
+
     for news in news_list:
         if news_exists(news.title):
             print(f"This news({news.title}) already registered in db")
         else:
             add_news(news.title, news.link)
             print(f"{news.title} added")
+
     for news in read_unprocessed_news():
         print(f"Start to process news({news.title})")
+
         raw_content = get_content(news.link)
         news.content = raw_content
         updated_content = translate_and_summarize(news)
-        new_title = get_new_title(news.title, updated_content)
+        new_title = get_new_title(news.title, updated_content).replace('"', "")
+
         print("new_title: ", new_title)
         print("updated_news: ", updated_content)
+
         sentences = updated_content.split(". ")
         dall_e_3_client = DallE3()
         final_clips = []
+
         for idx, sentence in enumerate(sentences):
-            audio_output_path = f"./outputs/audios/{new_title}_{idx}.mp3"
-            # if os.path.exists(audio_output_path):
-            #     print(f"Audio:{audio_output_path} already exists")
+            audio_output_dir = "./outputs/audios/"
+            image_output_dir = "./outputs/images/"
+            video_output_dir = "./outputs/videos/"
+            os.makedirs(audio_output_dir, exist_ok=True)
+            os.makedirs(image_output_dir, exist_ok=True)
+            os.makedirs(video_output_dir, exist_ok=True)
+
+            audio_output_path = os.path.join(audio_output_dir, f"{new_title}_{idx}.mp3")
             generate_audio(sentence, audio_output_path)
-            image_output_path = f"./outputs/images/{new_title}_{idx}.png"
-            if os.path.exists(image_output_path):
-                print(f"Image:{image_output_path} already exists")
-            else:
+
+            image_output_path = os.path.join(image_output_dir, f"{new_title}_{idx}.png")
+            if not os.path.exists(image_output_path):
                 response = dall_e_3_client.get_image_data(sentence)
                 image_data = response.b64_json
                 save_image(image_data, image_output_path)
+
             audio_clip = AudioFileClip(audio_output_path)
             image_clip = ImageClip(image_output_path).set_duration(audio_clip.duration)
-            title_clip = TextClip(
-                new_title,
-                fontsize=60,
-                color="white",
-                bg_color="black",
-                font="Malgun-Gothic-Bold",
-                size=(1024, 100),
-            ).set_duration(audio_clip.duration)
+
+            title_clip = TextClip(new_title, fontsize=60, color="white", bg_color="black", font="Malgun-Gothic-Bold", size=(1024, 100)).set_duration(audio_clip.duration)
             title_clip = title_clip.set_position(("center", "top"))
-            subtitle_clip = TextClip(sentence + ".", fontsize=24, color='white', bg_color='black', font="Malgun-Gothic").set_duration(audio_clip.duration)
-            subtitle_clip = subtitle_clip.set_pos('bottom')
-            # Set audio to image clip
-            video_clip = image_clip.set_audio(audio_clip)
-            # Overlay subtitle on video
-            video_clip = CompositeVideoClip([video_clip, title_clip, subtitle_clip])
-            # Write individual clip to file
-            video_output_path = f"./outputs/videos/{new_title}_{idx}.mp4"
+
+            subtitle_1, subtitle_2 = split_text(sentence)
+            subtitle_clip_1 = TextClip(subtitle_1, fontsize=35, color='white', bg_color='black', font="Malgun-Gothic").set_duration(audio_clip.duration).set_pos(('center', image_clip.size[1] - 90))
+            subtitle_clip_2 = TextClip(subtitle_2, fontsize=35, color='white', bg_color='black', font="Malgun-Gothic").set_duration(audio_clip.duration).set_pos(('center', image_clip.size[1] - 45))
+
+            video_clip = image_clip.set_audio(audio_clip).set_audio(audio_clip)
+            video_clip = CompositeVideoClip([video_clip, title_clip, subtitle_clip_1, subtitle_clip_2]) 
+
+            video_output_path = os.path.join(video_output_dir, f"{new_title}_{idx}.mp4")
             video_clip.write_videofile(video_output_path, fps=24)
-            # Append to final clips list
             final_clips.append(video_clip)
-        # Concatenate all individual clips
+
         merged_clip = concatenate_videoclips(final_clips)
+        merged_clip.write_videofile(os.path.join(video_output_dir, f"{new_title}.mp4"), fps=24)
 
-        # Write the final merged video to a file
-        merged_clip.write_videofile(f"outputs/videos/{new_title}.mp4", fps=24)
-        break
-        #process_news(news.title)
-
-
-def read_unprocessed_news():
-    query = "SELECT title, url FROM svt_news WHERE processed = 0"
-    return [News(title, "", url) for title, url in execute_query(query)]
+        process_news(news.title)
 
 if __name__ == "__main__":
     main()
